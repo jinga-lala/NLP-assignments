4fb4d62a72bebd04c38df54ad83b13069ce3491c
codejaeger <mandaldebabrata123@gmail.com>
2020-09-08 14:45:09 +0530

    Finalize code with library

--- Assignment 1/svm/svm.py
+++ Assignment 1/svm/svm.py
@@ -1,10 +1,16 @@
+import gensim
+from gensim.test.utils import get_tmpfile
+from gensim.models import KeyedVectors
+
 import nltk
 from nltk.corpus import brown
 from nltk.stem.snowball import SnowballStemmer
+
 from sklearn.feature_extraction import DictVectorizer
-from sklearn.svm import LinearSVC
+from sklearn.svm import LinearSVC, SVC
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
+
 import random
 import numpy as np
 
@@ -24,10 +30,15 @@ def getTaggedWords():
 
 	return nltk.corpus.brown.tagged_words(tagset='universal')
 
-
 def getTaggedSent():
 	return nltk.corpus.brown.tagged_sents(categories= nltk.corpus.brown.categories(), tagset='universal')
 
+def getSent():
+	return nltk.corpus.brown.sents()
+
+def get_word_embedding_model():
+	model = gensim.models.Word2Vec(getSent(), size=100, window=5, min_count=1, workers=4)
+	return model
 
 def Parition_in_n(input_list, n):
     random.shuffle(input_list)
@@ -64,9 +75,10 @@ def getData(num_cross_valid):
 	# print(Set_of_Words[0:100], Set_of_Tags)
 	# print(Dataset_Partition[0][0])
 
-def getFeatureData(sentence_dataset, vectorizer = DictVectorizer(), refit = True):
+def getFeatureData(sentence_dataset, word_vect, vectorizer = DictVectorizer(), refit = True):
 	stemmer = SnowballStemmer("english", ignore_stopwords=True)
 	feature_vec = []
+	word_embeds = []
 	pos_tags = []
 
 	for sent in sentence_dataset:
@@ -74,7 +86,7 @@ def getFeatureData(sentence_dataset, vectorizer = DictVectorizer(), refit = True
 			features = {
 			# 'prev-word' : '' if i==0 else sent[i-1][0],
 			'prev-tag' : '' if i==0 else sent[i-1][1],
-			# 'word' : stemmer.stem(sent[i][0]),
+			# 'word' : word_vect[sent[i][0]],
 			# 'tag' : sent[i][1],
 			# 'next-word' : '' if i==len(sent)-1 else sent[i+1][0],
 			'next-tag' : '' if i==len(sent)-1 else sent[i+1][1],
@@ -82,12 +94,12 @@ def getFeatureData(sentence_dataset, vectorizer = DictVectorizer(), refit = True
 			'is_last' : i==len(sent)-1,
 			'is_capitalized' : sent[i][0][0].upper == sent[i][0][0],
 			'is_numeric' : sent[i][0].isdigit(),
-			'prefix-1' : sent[i][0][0],
-	        'prefix-2' : '' if len(sent[i][0]) < 2  else sent[i][0][:1],
-	        'suffix-1' : sent[i][0][-1],
-	        'suffix-2' : '' if len(sent[i][0]) < 2  else sent[i][0][-2:]
+			# 'prefix-1' : sent[i][0][0],
+			'prefix-2' : '' if len(sent[i][0]) < 2  else sent[i][0][:1],
+			# 'suffix-1' : sent[i][0][-1],
+			'suffix-2' : '' if len(sent[i][0]) < 2  else sent[i][0][-2:]
 			}
-
+			word_embeds.append(word_vect[sent[i][0]])
 			feature_vec.append(features)
 			pos_tags.append(sent[i][1])
 	print("Done")
@@ -95,11 +107,12 @@ def getFeatureData(sentence_dataset, vectorizer = DictVectorizer(), refit = True
 	if refit:
 		vectorizer.fit(feature_vec)
 	
-	vectorized_features = vectorizer.transform(feature_vec)
-	print(len(vectorized_features.toarray()[0]))
-	return vectorizer, vectorized_features, pos_tags
+	vectorized_features = vectorizer.transform(feature_vec).toarray()
+	all_features = [np.array(list(vectorized_features[i])+list(word_embeds[i])) for i in range(len(word_embeds))]
+	print(len(all_features[0]))
+	return vectorizer, np.array(all_features), np.array(pos_tags)
 
-def train(num_cross_valid):
+def train(num_cross_valid, word_vect):
 	dataset, words, tags = getData(num_cross_valid)
 
 	for k in range(num_cross_valid):
@@ -114,19 +127,23 @@ def train(num_cross_valid):
 			else:
 				test_set = test_set + dataset[j]
 
-		vectorizer, feature_vecs, pos_tags = getFeatureData(train_set[0:10000])
+		vectorizer, feature_vecs, pos_tags = getFeatureData(train_set[0:2500], word_vect)
 		
 		print("Training started")
-		linear = make_pipeline(StandardScaler(), LinearSVC(tol=1e-3, random_state=0, max_iter=50, verbose=1, dual=False))
-		linear.fit(feature_vecs.toarray(), np.array(pos_tags))
-		# linear = svm.SVC(kernel='linear', C=1, decision_function_shape='ovr', verbose=1).fit(feature_vecs, pos_tags)
+		
+
+		# linear = make_pipeline(StandardScaler(), LinearSVC(tol=1e-3, random_state=0, max_iter=50, verbose=1, dual=False))
+		
+
+		# linear.fit(feature_vecs, pos_tags)
+		linear = SVC(kernel='linear', C=1, decision_function_shape='ovr', verbose=1).fit(feature_vecs, pos_tags)
 		print("Training ended")
 
-		_, test_vecs, test_pos = getFeatureData(test_set[0:1000], vectorizer, False)
+		_, test_vecs, test_pos = getFeatureData(test_set[0:1000], word_vect, vectorizer, False)
 
-		linear_pred = linear.predict(test_vecs.toarray())
+		linear_pred = linear.predict(test_vecs)
 		print(sum(linear_pred == test_pos), len(test_pos))
-		accuracy_lin = linear.score(test_vecs.toarray(), test_pos)
+		accuracy_lin = linear.score(test_vecs, test_pos)
 		print('Accuracy Linear Kernel:', accuracy_lin)
 
 		# print(len(feature_vecs.toarray()[0]), pos_tags[0])
@@ -139,6 +156,17 @@ def train(num_cross_valid):
 
 if __name__ == '__main__':
 	importdata()
-	train(5)
+
+	# Run once
+	# model = get_word_embedding_model()
+	# word_vect = model.wv
+	# print(word_vect['the'])
+	
+	fname = get_tmpfile("word_vector.kv")
+	# word_vect.save(fname)
+	word_vect = KeyedVectors.load(fname, mmap='r')
+	# print(word_vect['the'])
+	train(5, word_vect)
+
 	
 
