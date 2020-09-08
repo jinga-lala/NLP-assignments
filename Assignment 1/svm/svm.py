0d770d3e9f7787f32a9d6ae095815203d8da13ec
codejaeger <mandaldebabrata123@gmail.com>
2020-09-08 18:31:53 +0530

    Add svm implementation

--- Assignment 1/svm/svm.py
+++ Assignment 1/svm/svm.py
@@ -13,6 +13,7 @@ from sklearn.preprocessing import StandardScaler
 
 import random
 import numpy as np
+import time
 
 def importdata():
 	nltk.download('brown')
@@ -114,6 +115,7 @@ def getFeatureData(sentence_dataset, word_vect, vectorizer = DictVectorizer(), r
 
 def train(num_cross_valid, word_vect):
 	dataset, words, tags = getData(num_cross_valid)
+	print(len(tags))
 
 	for k in range(num_cross_valid):
 		print(len(dataset[k]))
@@ -126,10 +128,10 @@ def train(num_cross_valid, word_vect):
 				train_set = train_set + dataset[j]
 			else:
 				test_set = test_set + dataset[j]
-		vectorizer, feature_vecs, pos_tags = getFeatureData(train_set[0:1000], word_vect)
-		_, test_vecs, test_pos = getFeatureData(test_set[0:300], word_vect, vectorizer, False)
+		vectorizer, feature_vecs, pos_tags = getFeatureData(train_set[0:150], word_vect)
+		_, test_vecs, test_pos = getFeatureData(test_set[0:50], word_vect, vectorizer, False)
 		
-		print("Training started")
+		print("Training started len feature", len(feature_vecs), len(test_vecs))
 		
 
 		# linear = make_pipeline(StandardScaler(), LinearSVC(tol=1e-3, random_state=0, max_iter=50, verbose=1, dual=False))
@@ -139,6 +141,7 @@ def train(num_cross_valid, word_vect):
 		# O vs R
 
 		tag_classifiers = []
+		ws = []
 		tags2 = []
 		for cnt, k in enumerate(tags):
 			tg_d = []
@@ -146,13 +149,15 @@ def train(num_cross_valid, word_vect):
 			for l in pos_tags:
 				if l==k:
 					vl += 1
-					tg_d.append(0)
+					tg_d.append(-1)
 				else:
 					tg_d.append(1)
 			if vl == 0:
 				continue
 			tags2.append(k)
-			print("Tag ", k, " training started. Num tags", vl)
+			print("Tag ", k, cnt, " training started. Num tags", vl)
+			ws.append(svm_train(feature_vecs, tg_d))
+			print("Internediate training for", cnt)
 			svm_cl = SVC(kernel='linear', C=1, verbose=1, probability=True).fit(feature_vecs, tg_d)
 			print("Tag ", k, " training finished")
 			tag_classifiers.append(svm_cl)
@@ -163,21 +168,33 @@ def train(num_cross_valid, word_vect):
 
 		print("Test started")
 		outs = []
+		outs1 = []
 		for cnt, _ in enumerate(tags2):
 			print("Part Test started for ", cnt)
+			outs1.append(predict(ws[cnt], test_vecs))
+			print("Intermediate test for", cnt)
 			outs.append(tag_classifiers[cnt].predict_proba(test_vecs))
 		print("Part Test finished")
 
 		outs = np.array(outs)
+		outs1 = np.array(outs1)
+		print("results")
+		print(outs[0], outs1[0], test_pos[0])
+
+
 		out_tags = []
+		out_tags1 = []
+
 		for cnt, k in enumerate(test_vecs):
 			# print("Tagging started for ", cnt)
+			out_tags1.append(tags2[np.argmax(outs1[:, cnt, 0])])
 			out_tags.append(tags2[np.argmax(outs[:, cnt, 0])])
 		print("Output started")
 
 
 		linear_pred = linear.predict(test_vecs)
 		print(sum(linear_pred == test_pos), len(test_pos))
+		print(sum(out_tags1 == test_pos), len(test_pos))
 		print(sum(out_tags == test_pos), len(test_pos))
 		accuracy_lin = linear.score(test_vecs, test_pos)
 		print('Accuracy Linear Kernel:', accuracy_lin)
@@ -188,6 +205,81 @@ def train(num_cross_valid, word_vect):
 
 
 		#Test accuracy
+
+def svm_train(x, y):
+	#add bias to sample vectors
+	x = np.c_[x,np.ones(len(x))]
+
+	#initialize weight vector
+	w = np.zeros(len(x[0]))
+	#learning rate 
+	lam = 0.001
+	#array of number for shuffling
+	order = np.arange(0,len(x),1)
+	print(len(order))
+
+	margin_current = 0
+	margin_previous = -10
+
+	pos_support_vectors = 0
+	neg_support_vectors = 0
+
+	not_converged = True
+	t =0 
+	start_time = time.time()
+
+	while(not_converged):
+		margin_previous = margin_current
+		t += 1
+		pos_support_vectors = 0
+		neg_support_vectors = 0
+		
+		eta = 1/(lam*t)
+		fac = (1-(eta*lam))*w
+		random.shuffle(order)
+		for i in order:
+			# print(i, " i")
+			prediction = np.dot(x[i],w)
+			
+			#check for support vectors
+			if (round((prediction),1) == 1):
+				pos_support_vectors += 1
+				#pos support vec found
+			if (round((prediction),1) == -1):
+				neg_support_vectors += 1
+				#neg support vec found
+				
+			#misclassification
+			if (y[i]*prediction) < 1 :
+				w = fac + eta*y[i]*x[i]            
+			#correct classification
+			else:
+				w = fac
+		if(t%200==0):
+			print(t, " t")
+		if(t>2400):
+			break
+		if(t>1600):
+			margin_current = np.linalg.norm(w)
+			# not_converged = False    
+			if((pos_support_vectors > 0) and (neg_support_vectors > 0) and ((margin_current - margin_previous) < 0.01)):
+				not_converged = False
+	#print running time
+	print("--- %s seconds ---" % (time.time() - start_time))
+	return w
+	pass
+
+
+def predict(w, x_test):
+	y_pred = []
+	x_test = np.c_[x_test,np.ones(len(x_test))]
+	for i in x_test:
+		pred = np.dot(w,i)
+		if(pred > 0):
+			y_pred.append(np.array([0, 1]))
+		elif(pred < 0):
+			y_pred.append(np.array([1, 0]))
+	return y_pred
 
 
 if __name__ == '__main__':
